2f6f2d77edb298b61d6fa549313917bc1acc45ac
James <jamestan21403@gmail.com>
2026-01-23 18:51:03 +0800

    - Updated to include token for authorization

--- inventory-frontend/src/http-common.js
+++ inventory-frontend/src/http-common.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
+import { authState as auth } from './store/auth';
 
 // To define base settings for HTTP requests to the backend API
 export default axios.create({
   baseURL: "http://localhost:8080/api",
   headers: {
-    "Content-type": "application/json"
+    "Content-type": "application/json",
+    Authorization: `Bearer ${auth.token}`
   }
 });

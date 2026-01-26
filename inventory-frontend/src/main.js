95012e0e83f3fdab5acd1c243ac557a4d74cae61
James <jamestan21403@gmail.com>
2026-01-26 11:59:59 +0800

    Added validate token - on app startup

--- inventory-frontend/src/main.js
+++ inventory-frontend/src/main.js
@@ -1,6 +1,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { authState as auth } from './store/Auth.js';
+import http from "./http-common";
+
+// Validate token on app startup
+const token = localStorage.getItem('token');
+
+if (token) {
+    http.get('/auth/validate-token')
+        .then(() => {
+            // Token is valid, do nothing
+        })
+        .catch(() => {
+            localStorage.removeItem('token');
+            auth.logout();
+        });
+}
 
 createApp(App)
     .use(router)
